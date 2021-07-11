15fe2c42bc6c58a444c77127528454b8d2148513
Ami-Kimura <82437518+Ami-Kimura@users.noreply.github.com>
2021-07-11 20:38:32 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,109 @@
+// Window幅に応じたメニュー表示の変更
+function ShowAndHide(win) {
+  if (win < 769) {
+    $(".navButton").show();
+    $("#nav-menu").hide();
+  } else {
+    $(".navButton").hide();
+    $("#nav-menu").show();
+  }
+}
+
+
+//Smooth Scrool
+
+$(function() {
+
+  $('a[href^="#WORKS"]').click(function() {
+
+    // 初期設定：移動時間(ms)と頭出し位置
+    var speed = 500;
+    var offset = -0;
+
+    // アンカーを取得
+    var anchor = $(this).attr("href");
+
+    // ターゲットの位置を取得
+    var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
+    var position = target.offset().top + offset;
+
+    // スクロール（アニメーション）
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+
+    return false;
+
+  });
+
+});
+
+$(function() {
+
+  $('a[href^="#ABOUT"]').click(function() {
+
+    // 初期設定：移動時間(ms)と頭出し位置
+    var speed = 500;
+    var offset = -0;
+
+    // アンカーを取得
+    var anchor = $(this).attr("href");
+
+    // ターゲットの位置を取得
+    var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
+    var position = target.offset().top + offset;
+
+    // スクロール（アニメーション）
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+
+    return false;
+
+  });
+
+});
+
+$(function() {
+
+  $('a[href^="#CONTACT"]').click(function() {
+
+    // 初期設定：移動時間(ms)と頭出し位置
+    var speed = 500;
+    var offset = -0;
+
+    // アンカーを取得
+    var anchor = $(this).attr("href");
+
+    // ターゲットの位置を取得
+    var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
+    var position = target.offset().top + offset;
+
+    // スクロール（アニメーション）
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
+
+    return false;
+
+  });
+
+});
+
+
+$(function() {
+
+  // 初めてページを開いた時の状態チェック
+  ShowAndHide($(window).width());
+
+  // Windowサイズが変更された時の状態チェック
+  $(window).resize(function() {
+    ShowAndHide($(window).width());
+  });
+
+  // メニューボタンクリック時のトグル動作
+  $(".navButton").click(function() {
+    $("#nav-menu").slideToggle();
+  });
+
+});
