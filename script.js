75015d38cf323e11082bd740551dd6b4c23af4fb
Ami-Kimura <82437518+Ami-Kimura@users.noreply.github.com>
2021-07-24 20:35:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,32 @@
- 
-//まだ未完成なのでそのままコピーすると欠陥がでます
-  
-//欠陥だらけです
-  
-//ご注意下さい
-  
-//by Ami Kimura
-
-
 // Smooth Scrool
 
-$(function(){
+$(function() {
 
-   $('a[href^="#"]').click(function() {
+  $('a[href^="#"]').click(function() {
 
-      // 初期設定：移動時間(ms)と頭出し位置
-      var speed = 1200;
-      var offset = -104;
+    // 初期設定：移動時間(ms)と頭出し位置
+    var speed = 1200;
+    var offset = -150;
 
-      // アンカーを取得
-      var anchor = $(this).attr("href");
+    // アンカーを取得
+    var anchor = $(this).attr("href");
 
-      // ターゲットの位置を取得
-      var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
-      var position = target.offset().top + offset;
+    // ターゲットの位置を取得
+    var target = $(anchor == "#" || anchor == "" ? 'html' : anchor);
+    var position = target.offset().top + offset;
 
-      // スクロール（アニメーション）
-      $('body,html').animate({scrollTop:position}, speed, 'swing');
+    // スクロール（アニメーション）
+    $('body,html').animate({
+      scrollTop: position
+    }, speed, 'swing');
 
-      return false;
+    return false;
 
-   });
+  });
+
+});
+
+$('.dummy').click(function() {
+  return false;
 
 });
